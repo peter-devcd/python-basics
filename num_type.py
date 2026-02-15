e123ce07c1f3539dfb51dd627eb69917bf7de8de
peter-devcd <mrraj78877@gmail.com>
2026-02-15 19:42:58 +0530

    Add files via upload

---
+++ num_type.py
@@ -0,0 +1,10 @@
+#TO CHECK NUMBER TYPE.
+
+n=float(input("Enter a number:"))
+
+if n<0:
+	print("It's a negative number.")
+elif n>0:
+	print("It's a positive number.")
+else:
+	print("Number 0 entered")
