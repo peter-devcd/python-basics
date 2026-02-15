9eb540a0ec12f04228552b2024e7f68594cfa5a1
peter-devcd <mrraj78877@gmail.com>
2026-02-15 19:22:57 +0530

    Add files via upload

--- factorial.py
+++ factorial.py
@@ -1,5 +1,12 @@
 n=int(input("enter a number:"))
-fact=1;
-for i in range(1,n+1):
-	fact=fact*i
-print("factorial is:",fact)
+
+if n<0:
+	print("Factorial can't be calculated for negative numbers.")
+elif n == 0:
+	print("Factorial is 1.")
+else:
+
+	fact=1;
+	for i in range(1,n+1):
+		fact=fact*i
+	print("factorial is:",fact)
